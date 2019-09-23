de2c34443acd0a56a64495ecc8d99afca8f72fb9
sonic6 <alshihabi.ahmad@gmail.com>
2019-09-23 14:34:48 +0200

    added new things
    added ai bot script
    round placeholder race track
    added skybox
    added navmesh scripts

---
+++ Recycler Racer/Assets/Scripts/AiCart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class AiCart : MonoBehaviour
+{
+    public NavMeshAgent myCart;
+    public Transform triggerBox;
+
+    // Update is called once per frame
+    void Update()
+    {
+        myCart.SetDestination(triggerBox.position);
+    }
+}
--- Recycler Racer/Assets/Scripts/CartController.cs
+++ Recycler Racer/Assets/Scripts/CartController.cs
@@ -6,8 +6,10 @@ public class CartController : MonoBehaviour
 
     [Tooltip("racing cart's speed")]
     [SerializeField] float speed;
-
+    [Tooltip("racing cart's steering left and right speed")]
     [SerializeField] float steer;
+
+    bool onGround;
 
     void Start()
     {
@@ -16,16 +18,59 @@ public class CartController : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetAxis("Vertical") > 0)
-            myRig.AddRelativeForce(new Vector3(0, 0, speed) * Time.deltaTime, ForceMode.Impulse);
-        
-        else if(Input.GetAxis("Vertical") < 0)
-            myRig.AddRelativeForce(new Vector3(0, 0, -speed) * Time.deltaTime, ForceMode.Impulse);
+        StartDriving();
+    }
 
-        if (Input.GetAxis("Horizontal") > 0)
-            transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y + steer, transform.rotation.z), Space.Self);
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.tag == "floor")
+        {
+            onGround = true;
+        }
+    }
 
-        else if (Input.GetAxis("Horizontal") < 0)
-            transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y - steer, transform.rotation.z), Space.Self);
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "floor")
+        {
+            onGround = false;
+        }
+    }
+
+    private void StartDriving()
+    {
+        bool isDriving = false;
+        var cartVelocity = gameObject.GetComponent<Rigidbody>().velocity;
+
+        //if the cart is on the ground then it's allowed to drive forward or backwards
+        if (onGround)
+        {
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                myRig.AddRelativeForce(new Vector3(0, 0, speed) * Time.deltaTime, ForceMode.Impulse);
+                
+            }
+
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                myRig.AddRelativeForce(new Vector3(0, 0, -speed) * Time.deltaTime, ForceMode.Impulse);
+                
+            }
+        }
+
+        //if cart's velocity on x or z is not 0 then the cart is marked as driving
+        if (cartVelocity.x != 0 || cartVelocity.z != 0)
+            isDriving = true;
+        else isDriving = false;
+
+        //If the cart is driving (velocity doesn't equal 0) then you can steer left and right
+        if (isDriving == true)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+                transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y + steer, transform.rotation.z), Space.Self);
+
+            else if (Input.GetAxis("Horizontal") < 0)
+                transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y - steer, transform.rotation.z), Space.Self);
+        }
     }
 }
