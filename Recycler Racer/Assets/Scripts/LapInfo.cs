2d30d9615f6c703a8730c8c356f2dc94991bd965
sonic6 <alshihabi.ahmad@gmail.com>
2019-10-28 23:06:29 +0100

    update
    working on trackTargets script keeps track of player laps done

--- Recycler Racer/Assets/Scripts/LapInfo.cs
+++ Recycler Racer/Assets/Scripts/LapInfo.cs
@@ -7,10 +7,12 @@ public class LapInfo : MonoBehaviour
     GameObject goal;
     int lapNr = 0;
     RankList myList;
+    Transform[] trackPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        trackPos = FindObjectOfType<TrackTargets>().GetComponentsInChildren<Transform>();
         myList = FindObjectOfType<RankList>();
         goal = FindObjectOfType<RankList>().gameObject;
     }
@@ -19,13 +21,18 @@ public class LapInfo : MonoBehaviour
     {
         if(other.gameObject == goal)
         {
-            lapNr++;
-            if (lapNr == 3)
-            {
-                RankList.rank++;
-                myList.RegisterWin(this);
-            }
+            //lapNr++;
+            //if (lapNr == 3)
+            //{
+            //    RankList.rank++;
+            //    myList.RegisterWin(this);
+            //}
+
+            if (gameObject.layer == 8 && TrackTargets.nxtTarget == trackPos[trackPos.Length - 1])
+                print("win");
         }
         
     }
+
+
 }
--- Recycler Racer/Assets/Scripts/TrackTargets.cs
+++ Recycler Racer/Assets/Scripts/TrackTargets.cs
@@ -5,11 +5,22 @@ public class TrackTargets : MonoBehaviour
 {
     public Color color;
     public List<Transform> nodes = new List<Transform>();
+    Transform[] targets;
+    [Tooltip("insert here a Gameobject that will be used to identify the center position of the race track")]
+    public Transform trackCenter;
+    public static Transform nxtTarget;
+    int nxtInt = 1;
+
+    private void Start()
+    {
+        targets = GetComponentsInChildren<Transform>();
+        nxtTarget = targets[1];
+    }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = color;
-        Transform[] targets = GetComponentsInChildren<Transform>();
+        targets = GetComponentsInChildren<Transform>();
         nodes = new List<Transform>();
 
         for(int i = 0; i < targets.Length; i++)
@@ -31,5 +42,33 @@ public class TrackTargets : MonoBehaviour
             Gizmos.DrawLine(previousNode, currentNode);
             Gizmos.DrawWireSphere(currentNode, 0.5f);
         }
+    }
+
+    public void RayThroughTargets()
+    {
+        
+        foreach(Transform target in targets)
+        {
+            Vector3 rayStart = new Vector3(trackCenter.position.x, target.position.y, trackCenter.position.z);
+            Vector3 rayFinish = new Vector3(target.position.x, target.localPosition.y, target.position.z);
+            Ray ray = new Ray(rayStart, rayFinish);
+            RaycastHit hit;
+            Debug.DrawRay(rayStart, rayFinish, Color.green);
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.layer == 8 /*Layer nr 8 is the player layer*/ && targets[nxtInt] == target.transform)
+            {
+                if (nxtInt < targets.Length - 1)
+                    nxtInt++;
+                else
+                    nxtInt = 1;
+                nxtTarget = targets[nxtInt];
+            }
+        }
+        
+    }
+
+    private void Update()
+    {
+        RayThroughTargets();
     }
 }
