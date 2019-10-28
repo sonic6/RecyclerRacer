d82cbe76c85487fc9d69be3045f15d88b89472f1
sonic6 <alshihabi.ahmad@gmail.com>
2019-10-28 17:23:50 +0100

    cart selection script

---
+++ Recycler Racer/Assets/Scripts/CartSelection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartSelection : MonoBehaviour
+{
+    [SerializeField] GameObject[] Carts;
+    private GameObject currentCart;
+    private Transform previewPos;
+    int currentIndex = 0;
+
+    private void Start()
+    {
+        currentCart = Carts[currentIndex];
+        previewPos = currentCart.transform;
+    }
+
+    public void RightButton()
+    {
+        currentIndex++;
+        if (currentIndex < Carts.Length)
+            currentCart = Carts[currentIndex];
+        else
+        {
+            currentIndex = 0;
+            currentCart = Carts[currentIndex];
+        }
+    }
+
+    public void LeftButton()
+    {
+        currentIndex--;
+        if (currentIndex >= 0)
+            currentCart = Carts[currentIndex];
+        else
+        {
+            currentIndex = Carts.Length - 1;
+            currentCart = Carts[currentIndex];
+        }
+    }
+
+    //This function is unfinished. Will be continued
+    private void PreviewCart()
+    {
+        currentCart.transform.position = previewPos.position;
+    }
+}
