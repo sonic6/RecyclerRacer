58f2e9bab894da9f6de0907bd3ee1038d560af31
sonic6 <alshihabi.ahmad@gmail.com>
2019-12-12 19:06:11 +0100

    Updating game

---
+++ Recycler Racer/Assets/Scripts/EscapeMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeMenu : MonoBehaviour
+{
+    [SerializeField] GameObject menu;
+    float timeSpeed = 2.5f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ActivateMenu();
+    }
+    
+    private void ActivateMenu()
+    {
+        menu.SetActive(!(menu.activeSelf));
+        Time.timeScale = -(Time.timeScale - timeSpeed);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Recycler Racer/Assets/Scripts/LapEnd.cs
+++ Recycler Racer/Assets/Scripts/LapEnd.cs
@@ -4,13 +4,12 @@ using UnityEngine.UI;
 public class LapEnd : MonoBehaviour
 {
     [SerializeField] Text playerLapUi;
+    [SerializeField] Text[] positions;
+    int n = 0;
+    [SerializeField] GameObject positionsParent;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<LapInfo>() && other.GetComponent<LapInfo>().LapsDone == 2)
-        {
-            
-        }
 
         if(other.GetComponent<LapInfo>() && other.GetComponent<LapInfo>().lastLapTriggerPassed == 4)
         {
@@ -19,5 +18,50 @@ public class LapEnd : MonoBehaviour
             if (other.GetComponent<CartController>())
                 playerLapUi.text = other.GetComponent<LapInfo>().LapsDone.ToString();
         }
+
+        if (other.GetComponent<LapInfo>() && other.GetComponent<LapInfo>().LapsDone == 3)
+        {
+            if (other.GetComponent<CartController>())
+                other.GetComponent<CartController>().enabled = false;
+
+            if ( n==0 ) FirstPlace(other);
+            else if (n == 1) SecondPlace(other);
+            else if (n == 2) FinalPlace(other);
+        }
+    }
+
+    private void FirstPlace(Collider cart)
+    {
+        positions[n].text = "1st place: " + cart.gameObject.name;
+        n++;
+    }
+    private void SecondPlace(Collider cart)
+    {
+        positions[n].text = "2nd place: " + cart.gameObject.name;
+        n++;
+    }
+    private void FinalPlace(Collider cart)
+    {
+        positions[n].text = "3rd place: " + cart.gameObject.name;
+        n++;
+        FindLastCart();
+        positionsParent.SetActive(true);
+    }
+
+    private void FindLastCart()
+    {
+        GameObject[] cart = GameObject.FindGameObjectsWithTag("cart");
+        GameObject LastCart = null;
+
+        for(int x = 0; x < cart.Length; x++)
+        {
+            if (cart[x].GetComponent<LapInfo>().LapsDone < 3)
+                LastCart = cart[x]; 
+        }
+
+        positions[n].text = "4th place: " + LastCart.name;
+
+        if (LastCart.GetComponent<CartController>())
+            LastCart.GetComponent<CartController>().enabled = false;
     }
 }
